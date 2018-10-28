3a074185130f24fc8947464ccc782f646a8f1407
Mustaq Ahammad <43813438+mustaqmustu@users.noreply.github.com>
2018-10-28 15:33:05 +0530

    Create normal2.py

---
+++ gaussian distribution/normal2.py
@@ -0,0 +1,12 @@
+mu, sigma = 0.1, 0.1 # mean and standard deviation
+import numpy as np
+s = np.random.normal(mu, sigma, 1000)
+print(s)
+#gives you random distribution
+import matplotlib.pyplot as plt
+count, bins, ignored = plt.hist(s, 30, density=True)
+plt.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) *
+                np.exp( - (bins - mu)**2 / (2 * sigma**2) ),
+         linewidth=2, color='r')
+plt.show()
+#gives you histogram of above normal distribution
